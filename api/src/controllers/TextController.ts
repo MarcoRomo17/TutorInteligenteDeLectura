6b86bc61ada9b5333f81d8309021948264f2d961
MarcoRomo17 <utm23090604@utma.edu.mx>
2025-07-26 21:16:58 -0600

    Endpint para dministrar textos

--- api/src/controllers/TextController.ts
+++ api/src/controllers/TextController.ts
@@ -32,6 +32,22 @@ export const uploadText= async (req: Request, res: Response,): Promise<any> => {
     }
 }
 
+export const bringAllText= async (req: Request, res: Response,): Promise<any> => {
+    try {
+       const allTexts= textModel.find()
+
+       if(!allTexts){//configuramos un mensjae cuando no haya textos
+        return res.status(200).json({"msg":"No hubo error, no hay textos"})
+       }
+       return res.status(200).json({"msg":"Encontre estos textos:", allTexts})
+    } catch (error) {
+        return res.status(500).json({"msg":"Hubo un fallo del lado del servidor", "error":error})
+    }
+  
+}
+
+//el siguiente endpoint no se que tan idoneo sea. Se supone es el jale de la IA, pero como no se como funcione, por eso solo dejo el comentario.
+//-Marco
 export const getSugested = async (req:Request, res:Response):Promise<any>=>{
 
     try{
@@ -53,3 +69,59 @@ export const getSugested = async (req:Request, res:Response):Promise<any>=>{
         })
     }
 }
+
+export const updateText= async (req: Request, res: Response,): Promise<any> => {
+    try {
+        const id =req.body.id
+        const titleReceived = req.body.title 
+        const contentReceived = req.body.content
+        const estimatedLevelReceived = req.body.estimatedLevel 
+
+        if (!titleReceived || !contentReceived || !estimatedLevelReceived){
+            return res.status(400).json({
+                msg: "faltan datos para actualizar el texto"
+            })
+        }
+
+        const text = await textModel.findOneAndUpdate({_id:id}, 
+            {title:titleReceived, content:contentReceived, estimatedLevel:estimatedLevelReceived},
+            {new:true}
+        )
+
+        return res.status(200).json({
+            msg: "texto actualizado con exito",
+            "TituloActualizado":text?.title,
+            "nivelActualizado":text?.estimatedLevel,
+
+        })
+    } catch (error) {
+
+        console.log(error)
+        return res.status(500).json({
+            msg: "Hubo un error al actualizar el texto",
+        })
+    }
+}
+
+export const deleteText= async(req:Request, res:Response):Promise<any>=>{
+        try {
+            const id = req.body.id
+
+         if (!id){
+            return res.status(400).json({
+                msg: "falta el id"
+            })
+        }
+            const deleted = textModel.findOneAndDelete({_id:id})
+
+                  return res.status(200).json({"msg":"eliminado con exito"})
+
+        } catch (error) {
+            
+        console.log(error)
+        return res.status(500).json({
+            msg: "Hubo un error al actualizar el texto",
+        })
+        }
+}
+
--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -42,6 +42,47 @@ export const registerUser= async (req:Request, res: Response): Promise<any>=>{
         return res.status(500).json({msg:"Fallo al intentar registrar al conductor."})
     }
 }
+export const registerTeacher= async (req:Request, res: Response): Promise<any>=>{
+    try {
+        const {name,
+            ap,
+            am,
+            email,
+            password}=req.body;
+
+        //Validar que venga todo:
+        if(!name||
+            !ap||
+            !am||
+            !email||
+            !password){
+            return res.status(400).json({
+                msg:" faltan datos para registrar al user"
+            })                     //devolvemos un json
+        }
+
+
+        const rol= "Teacher"
+   
+
+        const registered= await userModel.create({
+            name,
+            ap,
+            am,
+            email,
+            password,
+            rol
+        })
+
+        return res.status(200).json({msg:"usuario registrado con exito.", registered})
+
+
+    } catch (error) {
+        console.log("Error al registrar al conductor")
+        console.log(error)
+        return res.status(500).json({msg:"Fallo al intentar registrar al conductor."})
+    }
+}
 
 export const allUsers= async (req:Request, res: Response): Promise<any>=>{
     try {
@@ -100,4 +141,34 @@ export const deleteUser = async (req:Request, res: Response): Promise<any>=>{
     }
 }
 
+export const loginUser= async (req:Request, res: Response): Promise<any>=>{
+    try {
+        const {email,
+            password}=req.body;
 
+        //Validar que venga todo:
+        if(!email||
+            !password){
+            return res.status(400).json({
+                msg:" faltan datos para logear al user"
+            })                     //devolvemos un json
+        }
+
+        const emailRecibido= email //Para que no haya confusion, el email que recibimos lo guardamos en una variable local
+        const passwordRecibida= password //Para que no haya confusion, el email que recibimos lo guardamos en una variable local
+
+         
+
+        const userLoged= await userModel.find({email:emailRecibido, password:passwordRecibida}).lean()//buscamos el usuario. Se supone el .lean() es para hacerlo objeto
+        if(!userLoged){
+            return res.status(400).json({msg:"No se encontro nada"})
+        }
+
+        return res.status(200).json({msg:"usuario encontrado con exito.", userLoged})
+        
+    } catch (error) {
+        console.log("Error al logar")
+        console.log(error)
+        return res.status(500).json({msg:"Fallo al intentar logear"})
+    }
+}
