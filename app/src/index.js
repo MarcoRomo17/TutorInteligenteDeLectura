403c0b25c7828446db597d3c309c0b9e87e4af45
JuanManuelGomezOchoa <utm23090605@utma.edu.mx>
2025-07-28 18:49:07 -0600

    Vista para ver resultados con datos HARDCODEADOS

--- app/src/index.js
+++ app/src/index.js
@@ -16,6 +16,7 @@ import { CreateUser } from "./CreateUser";
 import { CreateProfessor } from "./CreateProfessor";
 import { CreateText } from "./registerText";
 import { Catalogue } from "./TextCatalogue";
+import { Results } from "./Results";
 
 const router = createBrowserRouter([
   {
@@ -50,6 +51,10 @@ const router = createBrowserRouter([
     path:"/Catalogue",
     element:<Catalogue/>
   },
+  {
+    path:"/Results",
+    element:<Results/>
+  }
 
 ]);
 
