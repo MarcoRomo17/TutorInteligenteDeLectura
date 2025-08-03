950cfd6a742739242133dcefe4b0c285289bdb66
NIntenCarlos <juancarlossantoyo0@gmail.com>
2025-08-03 01:01:03 -0600

    Conexión de la IA al frontend del proyecto en general

--- app/src/index.js
+++ app/src/index.js
@@ -1,74 +1,73 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  
-} from "react-router-dom";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { CreateQuestion } from "./CreateQuestion";
 import { SeeingQuestions } from "./SeeingQuestions";
 import { Login } from "./Login";
-import Dashboard from './Dashboard';
+import Dashboard from "./Dashboard";
 import { CreateUser } from "./CreateUser";
 import { CreateProfessor } from "./CreateProfessor";
 import { CreateText } from "./registerText";
 import { Catalogue } from "./TextCatalogue";
 import { Results } from "./Results";
+import Chat from "./Chat";
 
 const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <App/>
-  },
-  {
-    path:"/CreateQuestion",
-    element: <CreateQuestion/>
-  },
-  {
-    path:"/SeeingQuestions",
-    element: <SeeingQuestions/>
-  },
-  {
-    path:"/Login",
-    element:<Login/>
-  },
-  {
-    path:"/CreateUser",
-    element:<CreateUser/>
-  },
-  {
-    path:"/CreateProfessor",
-    element:<CreateProfessor/>
-  },
-  {
-    path:"/registerText",
-    element:<CreateText/>
-  },
-  {
-    path:"/Catalogue",
-    element:<Catalogue/>
-  },
-  {
-    path:"/Results",
-    element:<Results/>
-  },
-  {
-    path:"/Dashboard",
-    element:<Dashboard/>
-  }
-
-
+   {
+      path: "/",
+      element: <App />,
+   },
+   {
+      path: "/CreateQuestion",
+      element: <CreateQuestion />,
+   },
+   {
+      path: "/SeeingQuestions",
+      element: <SeeingQuestions />,
+   },
+   {
+      path: "/Login",
+      element: <Login />,
+   },
+   {
+      path: "/CreateUser",
+      element: <CreateUser />,
+   },
+   {
+      path: "/CreateProfessor",
+      element: <CreateProfessor />,
+   },
+   {
+      path: "/registerText",
+      element: <CreateText />,
+   },
+   {
+      path: "/Catalogue",
+      element: <Catalogue />,
+   },
+   {
+      path: "/Results",
+      element: <Results />,
+   },
+   {
+      path: "/Dashboard",
+      element: <Dashboard />,
+   },
+   {
+      path: "/Chat",
+      element: <Chat />,
+   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-        <RouterProvider router={router}/>
-  </React.StrictMode>
+   <React.StrictMode>
+      <RouterProvider router={router} />
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
